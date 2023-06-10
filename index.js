141455a50947e50e568c843cf2491c0ad91430a6
asm <vaskocuturilo@rambler.ru>
2023-06-10 06:34:54 +0200

    DV-0001: First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorButton = document.querySelector('#scissor');
+const choiseButtons = document.querySelectorAll('.choiceButtons');
+
+choiseButtons.forEach(button => addEventListener("click"), ()=>{
+    alert(button);
+});
+    
